52bb5862116f6a3f3704e1017e0596ede4501cb8
yinhui.yh <yinhui.yh@alibaba-inc.com>
2019-09-10 00:40:45 +0800

    feat:支持额外配置文件，比如多个vpc部署

--- config/config.default.js
+++ config/config.default.js
@@ -4,25 +4,28 @@ const path = require('path');
 const pkgInfo = require('../package.json');
 
 module.exports = appInfo => {
-  const config = {};
 
-  /**
-   * 框架信息
-   * @member Config#framework
-   * @property {String} name 框架名称
-   * @property {String} version 框架版本
-   * @property {Object} pkg 框架信息
-   */
-  config.framework = {
-    name: 'bud',
-    version: pkgInfo.version,
-    pkg: pkgInfo,
-  };
+  return {
 
-  // 默认关闭 alinode
-  config.alinode = {
-    enable: false,
-  };
+    /**
+     * 框架信息
+     * @member Config#framework
+     * @property {String} name 框架名称
+     * @property {String} version 框架版本
+     * @property {Object} pkg 框架信息
+     */
+    framework : {
+      name: 'bud',
+      version: pkgInfo.version,
+      pkg: pkgInfo,
+    },
 
-  return config;
+    // 默认关闭 alinode
+    alinode : {
+      enable: false,
+    },
+
+    extConfPath : path.join(appInfo.baseDir, 'conf/config.js'),
+
+  }
 };
